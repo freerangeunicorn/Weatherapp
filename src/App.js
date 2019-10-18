264c7c97318378a16d40420ddb3fbf91db31c31d
Mai <mai.th.hoang@gmail.com>
2019-10-18 10:31:01 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Cities } from "./Cities";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [weather, setWeather] = useState(null);
+  const [photo, setPhoto] = useState(null);
+  const [cities, setCities] = useState(null);
+  const getWeatherLocation = () => {
+    navigator.geolocation.getCurrentPosition(post => {
+      getData(post.coords.longitude, post.coords.latitude);
+    });
+  };
+  useEffect(() => {
+    getWeatherLocation();
+  }, []); //to run once
+
+  const getData = async (long, lat) => {
+    const API_KEY = "84ae64078f1582569bc184c12a0343a1";
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`
+    );
+    const data = await response.json();
+    console.log(data);
+    getPhoto(data.name);
+    setWeather(data);
+  };
+
+  const getPhoto = async cityName => {
+    const API_KEY =
+      "7ea8d6acc1a47cb696150e7725573dbd9abb308607ab2866f29f0696fab9fa63";
+    const response = await fetch(
+      `https://api.unsplash.com/search/photos?query=${cityName}&page=1&client_id=${API_KEY}`
+    );
+    const data = await response.json();
+    console.log(cityName, data);
+    setPhoto(data.results[0].urls.raw);
+  };
+
+  const appStyle = {
+    backgroundPosition: "center",
+    backgroundSize: "contain",
+    backgroundImage: `url(${photo})`,
+    backgroundRepeat: "none"
+  };
+
+  function celsiusToF(number) {
+    return (number * 9) / 5 + 32;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={appStyle} className="container">
+      <div className="container-fluid text-white my-auto">
+        <div className="container mx-auto my-4 py-4">
+          <div className="row justify-content-center text-center">
+            <h1 className="col-12 display-4 my-2 py-3 text-dark">
+              Mai Weather App
+            </h1>
+            <h2 className="col-12">{weather && weather.name}</h2>
+            <h3 className="col-12 text-danger">
+              {weather && weather.main.temp}°C <br />
+              {weather && celsiusToF(weather.main.temp)}°F{" "}
+            </h3>
+            <h3 className="col-12 text-light">
+              <img
+                src={`https://openweathermap.org/img/wn/${weather &&
+                  weather.weather[0].icon}@2x.png`}
+                style={{ width: "50px", height: "50px" }}
+              ></img>
+              {weather && weather.weather[0].description}
+            </h3>
+            <div>
+          <ul><button type="button" onClick={()=>{getData(Cities[0].longitude,Cities[0].latitude)}}  class="btn btn-outline-light">
+          Stockholm
+        </button>
+        <button type="button" onClick={()=>{getData(Cities[1].longitude,Cities[1].latitude)}} class="btn btn-outline-light">
+          Los Angeles
+        </button>
+        <button type="button" onClick={()=>{getData(Cities[2].longitude,Cities[2].latitude)}}class="btn btn-outline-light">
+          Berlin
+        </button>
+        <button  type="button" onClick={()=>{getData(Cities[3].longitude,Cities[3].latitude)}} class="btn btn-outline-light">
+          Hong Kong
+        </button>
+        <button type="button" onClick={()=>{getData(Cities[4].longitude,Cities[4].latitude)}} class="btn btn-outline-light">
+          Bali
+        </button>
+        <button type="button" onClick={()=>{getData(Cities[5].longitude,Cities[5].latitude)}} class="btn btn-outline-light">
+          Kyiv
+        </button>
+        <button type="button" onClick={()=>{getData(Cities[6].longitude,Cities[6].latitude)}} class="btn btn-outline-light">
+          Goa
+        </button>
+        <button type="button" onClick={()=>{getData(Cities[7].longitude,Cities[7].latitude)}} class="btn btn-outline-light">
+          Ho Chi Minh City
+        </button>
+        <button type="button" onClick={()=>{getData(Cities[8].longitude,Cities[8].latitude)}}class="btn btn-outline-light">
+          Chiang Mai
+        </button>
+        </ul>
+        </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Cities.js
@@ -0,0 +1,42 @@
+
+export const Cities = [
+    {name: 'Stockholm',
+    latitude: 59.3293,
+    longitude: 18.0686},
+    
+    {name: 'Los Angeles',
+    latitude: 34.0522,
+    longitude: -118.2437},
+    
+    {name: 'Berlin',
+    latitude: 52.5200,
+    longitude: 13.4050},
+    
+    {name: 'Hong Kong', 
+    latitude: 22.2821,
+    longitude: 114.1568},
+    
+    {
+        name: 'Bali',
+        latitude: -8.3405,
+        longitude: 115.0920
+    },
+    
+    {name: 'Kyiv',
+    latitude:  30.523399,
+    longitude: 50.450100},
+    
+    {name: 'Goa',
+    latitude: 15.5028,
+    longitude: 73.9154},
+    
+    {name: 'Ho Chi Minh',
+    latitude:  10.7765,
+    longitude: 106.7009,},
+    
+    {name: 'Chiang Mai',
+    latitude: 18.7953,
+    longitude: 98.9620,}
+    
+    
+    ]
